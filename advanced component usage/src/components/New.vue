08cf8f9d7563922b9e7a9b950d4ededd00fc246f
Doanh Pham <doanhpham1995@gmail.com>
2019-01-21 19:36:31 -0500

    advanced component usage

---
+++ advanced component usage/src/components/New.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>The new</h1>
+    <button @click="counter++">Increase</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  destroyed() {
+    console.log("destroy New");
+  },
+  deactivated() {
+    console.log("Deactivated New");
+  },
+  activated() {
+    console.log("Activated New");
+  }
+};
+</script>
+
+<style scoped>
+</style>
