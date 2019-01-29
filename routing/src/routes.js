2afe51b3c9f5dc0ea2c4166c709c3cd6b8ac6c2a
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-01-29 14:48:15 -0500

    group lazy loading

--- routing/src/routes.js
+++ routing/src/routes.js
@@ -1,16 +1,25 @@
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
 
+// 'user' means group lazy loading components
 const User = resolve => {
-  require.ensure(['./components/user/User.vue'], () => {
-    resolve(require('./components/user/User.vue'));
-  });
+  require.ensure(
+    ['./components/user/User.vue'],
+    () => {
+      resolve(require('./components/user/User.vue'));
+    },
+    'user'
+  );
 };
 
 const UserStart = resolve => {
-  require.ensure(['./components/user/UserStart.vue'], () => {
-    resolve(require('./components/user/UserStart.vue'));
-  });
+  require.ensure(
+    ['./components/user/UserStart.vue'],
+    () => {
+      resolve(require('./components/user/UserStart.vue'));
+    },
+    'user'
+  );
 };
 
 const UserDetail = resolve => {
